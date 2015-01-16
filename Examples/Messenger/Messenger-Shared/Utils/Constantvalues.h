8b8191719609dd65f6ea82191dbe3f3e0e8ea51e
C.H Lee <gzonelee@gmail.com>
2015-01-16 20:39:58 +0900

    .

--- Examples/Messenger/Messenger-Shared/Utils/Constantvalues.h
+++ Examples/Messenger/Messenger-Shared/Utils/Constantvalues.h
@@ -36,18 +36,4 @@
 #define kFailed             @"Failed"
 
 
-
-//#ifdef DEBUG
-//#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
-//#else
-//#   define DLog(...)
-//#endif
-//
-//#if DEBUG
-//#define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
-//#else
-//#define NSLog(FORMAT, ...) nil
-//#endif
-
-
 #endif
